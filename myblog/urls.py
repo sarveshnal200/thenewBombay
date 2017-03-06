104a0cc0cc47fdfefca8db0f7e81594bd5801e8a
Sarvesh <sarveshnal@gmail.com>
2017-03-06 22:23:04 +0530

    Created the home page template and redirected urls accordingly

--- myblog/urls.py
+++ myblog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.home, name='home'),
+    url(r'^blog', views.post_list, name='post_list'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'myblog/post_list.html', {'posts': posts})
+    return render(request, 'myblog/blog.html', {'posts': posts})
+
+
+def home(request):
+    return render(request, 'myblog/index.html')
